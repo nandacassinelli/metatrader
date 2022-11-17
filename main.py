8107a5427096172db89de8740caf1c278f1382fd
Fernanda Cassinelli <fernandafbcassinelli@gmail.com>
2022-11-16 23:36:36 -0300

    Iniciando rota de compra de ações

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from meta_trader import initialize, login, get_account_info
+from meta_trader import initialize, login, get_account_info, send_order
 
 initialized = initialize()
 if not initialized:
@@ -12,24 +12,29 @@ class Credential(BaseModel):
     password: str
     server: str
 
+class OrderRequest(BaseModel):
+    symbol: str
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    logged = login()
-    if not logged:
-        print("Erro ao logar.")
-        quit()
-    info = get_account_info()
-    return info
+    return { 'message': 'Hello, this is a Metratrader API' }
 
 
-@app.post("/")
+@app.post("/login")
 async def login(credential: Credential):
     logged = login(credential)
     if not logged:
         print("Erro ao logar.")
         quit()
     info = get_account_info()
+    return info
+
+@app.post("/order")
+async def post_order(order_request: OrderRequest):
+    info = send_order(order_request)
+    if info is None:
+        info = { 'message': 'Internal Server Error' }
     return info
--- meta_trader.py
+++ meta_trader.py
@@ -33,5 +33,51 @@ def get_account_info(props = False):
         info[prop] = account_info_dict[prop]
     return info
 
+def send_order(request):
+    symbol = request.symbol
+    symbol_info = mt5.symbol_info(symbol)
+    if symbol_info is None:
+        print(symbol, "not found, can not call order_check()")
+        return None
+    if not symbol_info.visible:
+        print(symbol, "is not visible, trying to switch on")
+    if not mt5.symbol_select(symbol,True):
+        print("symbol_select({}}) failed, exit",symbol)
+        return None
+
+    point = mt5.symbol_info(symbol).point
+    price = mt5.symbol_info_tick(symbol).ask
+    request = {
+        "action": mt5.TRADE_ACTION_DEAL,
+        "symbol": symbol,
+        "volume": 1,
+        "type": mt5.ORDER_TYPE_BUY,
+        "price": price,
+        "sl": price - 100*point,
+        "tp": price + 100*point,
+        "deviation": 10,
+        "magic": 234000,
+        "comment": "python script",
+        "type_time": mt5.ORDER_TIME_GTC,
+        "type_filling": mt5.ORDER_FILLING_RETURN,
+    }
+    result = mt5.order_send(request)
+
+    if result is None:
+        print('No result info')
+        return None
+    if result.retcode != mt5.TRADE_RETCODE_DONE:
+        print("2. order_send failed, retcode={}".format(result.retcode))
+        result_dict=result._asdict()
+        for field in result_dict.keys():
+            print("   {}={}".format(field,result_dict[field]))
+            if field=="request":
+                traderequest_dict=result_dict[field]._asdict()
+                for tradereq_filed in traderequest_dict:
+                    print("       traderequest: {}={}".format(tradereq_filed,traderequest_dict[tradereq_filed]))
+
+    return { 'result': result, 'request': request, 'point': point }
+
+
 def shutdown():
     mt5.shutdown()
